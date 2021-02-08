85da7fdf9b65093e64ea2dc31b72079402b8c606
SkyVett <71993406+SufiRepository@users.noreply.github.com>
2021-02-08 20:54:03 +0800

    update operator

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,38 @@
+if not True:
+    print("1")
+elif not (1+1==3):
+    print("2")
+else:
+    print("3")
+
+
+
+
+# if 1 + 1 == 2:
+#  if 2 * 2 == 8:
+#   print("if")
+#  else:
+#   print("else")
+#
+# if 2 == 8:
+#     print("Hi")
+# elif 2 + 3 == 4:
+#     print("World")
+# else:
+#     print("else")
+
+# print("What your age")
+# age = int(input())
+# print(age+8)
+
+# x = 3
+# num = 17
+# print(num % x)
+# x = 5
+# y = x + 3
+# y = int(str(y) + "2")
+# print(y)
+
 # print('Hello World')
 
 # print(10 // 4)
@@ -7,6 +42,15 @@
 # print(4*'2')
 
 # print("Father's name Uwu")
-name = input("What is your name? ")
-color_fav = input("What is your fav color? ")
-print(name + " likes " + color_fav)
+# name = input("What is your name? ")
+# color_fav = input("What is your fav color? ")
+# print(name + " likes " + color_fav)
+
+# x = "3"
+# y = "2"
+# print(x+y)
+#
+# spam = "7"
+# spam = spam + "0"
+# eggs = int(spam) + 3
+# print(float(eggs))
